2a261ba9369e8a82047424914eb9b02a7ac3961c
zahin5542 <32746408+zahin5542@users.noreply.github.com>
2021-07-15 15:04:37 +0600

    jQuery plugins demo in one place

---
+++ js/custom.js
@@ -0,0 +1,68 @@
+
+
+
+/****jQuery conflict-fix*****/ 
+//$ = $myJQ
+jQuery.noConflict();
+
+
+/***owl *****/ 
+jQuery('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+});
+
+
+/***slick *****/ 
+
+jQuery(document).ready(function(){
+  jQuery('.myGallery').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay:true,
+        autoplaySpeed:2000,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true,
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
+    });
+  });
+
+
+
